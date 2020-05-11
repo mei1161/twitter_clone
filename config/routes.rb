ca12a1a1a3d333d708e97c85455c69726ea4ad34
mei1161 <ichigo_happy@ezweb.ne.jp>
2020-05-11 14:21:20 +0900

    Add email authentication function when creating a user

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   root 'welcome#index'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
+    confirmations: 'users/confirmations'
   }
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
