778728eba8732e1e3f5604527c5ed329202ce5d3
mei1161 <ichigo_happy@ezweb.ne.jp>
2020-05-11 12:39:48 +0900

    add views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
   root 'welcome#index'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
